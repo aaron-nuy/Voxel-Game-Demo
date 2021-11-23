323e60421eb05ab9185ac4a16ef9a27a10b0d776
aaron-nuy <94788370+aaron-nuy@users.noreply.github.com>
2021-11-23 20:33:33 +0100

    Update Polygone2D.cpp

--- src/abstractions/Polygone2D.cpp
+++ src/abstractions/Polygone2D.cpp
@@ -4,14 +4,14 @@ Polygone2D::Polygone2D(Texture* text, Shader* shade )
 {
 	texture = text;
 	shader = shade;
-	VAO.Bind();
-	VBO.Load(crossHairArrayVerts, sizeof(crossHairArrayVerts));
-	EBO.Load(crossHairArrayIndices, sizeof(crossHairArrayIndices));
-	VAO.LinkAttrib(VBO, 0, 2, GL_FLOAT, sizeof(GLfloat) * 4, (void*)0);
-	VAO.LinkAttrib(VBO, 1, 2, GL_FLOAT, sizeof(GLfloat) * 4, (void*)(2 * sizeof(GLfloat)));
-	VAO.Unbind();
-	VBO.Unbind();
-	EBO.Unbind();
+	_mVAO.Bind();
+	_mVBO.Load(crossHairArrayVerts, sizeof(crossHairArrayVerts));
+	_mVEBO.Load(crossHairArrayIndices, sizeof(crossHairArrayIndices));
+	_mVVAO.LinkAttrib(_mVVBO, 0, 2, GL_FLOAT, sizeof(GLfloat) * 4, (void*)0);
+	_mVVAO.LinkAttrib(_mVVBO, 1, 2, GL_FLOAT, sizeof(GLfloat) * 4, (void*)(2 * sizeof(GLfloat)));
+	_mVVAO.Unbind();
+	_mVVBO.Unbind();
+	_mVEBO.Unbind();
 }
 
 void Polygone2D::Draw(float aspect) {
@@ -20,6 +20,6 @@ void Polygone2D::Draw(float aspect) {
 	texture->Bind();
 	texture->Assign(*shader, "diffuse0", 1);
 
-	VAO.Bind();
+	_mVVAO.Bind();
 	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 }
