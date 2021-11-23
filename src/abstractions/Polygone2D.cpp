4ad31dcfc0c1c5e7376b77319576f41e82de8b41
aaron-nuy <94788370+aaron-nuy@users.noreply.github.com>
2021-11-23 22:51:26 +0100

    Update Polygone2D.cpp

--- src/abstractions/Polygone2D.cpp
+++ src/abstractions/Polygone2D.cpp
@@ -7,8 +7,8 @@ Polygone2D::Polygone2D(Texture* text, Shader* shade )
 	_mVAO.Bind();
 	_mVBO.Load(crossHairArrayVerts, sizeof(crossHairArrayVerts));
 	_mEBO.Load(crossHairArrayIndices, sizeof(crossHairArrayIndices));
-	_mVVAO.LinkAttrib(_mVBO, 0, 2, GL_FLOAT, sizeof(GLfloat) * 4, (void*)0);
-	_mVVAO.LinkAttrib(_mVBO, 1, 2, GL_FLOAT, sizeof(GLfloat) * 4, (void*)(2 * sizeof(GLfloat)));
+	_mVAO.LinkAttrib(_mVBO, 0, 2, GL_FLOAT, sizeof(GLfloat) * 4, (void*)0);
+	_mVAO.LinkAttrib(_mVBO, 1, 2, GL_FLOAT, sizeof(GLfloat) * 4, (void*)(2 * sizeof(GLfloat)));
 	_mVAO.Unbind();
 	_mVBO.Unbind();
 	_mEBO.Unbind();
